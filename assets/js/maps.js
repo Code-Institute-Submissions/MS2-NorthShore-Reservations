d0e16cfaa5d1f0d731885d3dc5b6d5436f8ee210
suping106 <lou_suping@yahoo.com>
2020-11-25 03:19:48 +0000

    map images info content and form added

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,5 +1,5 @@
     // Property array
-    var properties = [
+    const properties = [
         ['Appleton Farm', 42.647935, -70.850771,'appleton-sm.jpg', 'appleton-farms/', 1],
         ['Castle Hill Estate', 42.682783, -70.777461, 'castlehill-sm.jpg', 'castle-hill-on-the-crane-estate/', 2],
         ['Crane Beach', 42.683697, -70.766222, 'cranebeach-sm.jpg', 'crane-beach-on-the-crane-estate/', 3],
@@ -10,16 +10,22 @@
         ['Old Town Hill', 42.768704, -70.849503, 'oldtownhill-sm.jpg', 'old-town-hill/', 8],
         ['Ravenswood Park', 42.592897, -70.698277, 'Ravenswood-sm.jpg', 'ravenswood-park/', 9],
         ['Stavros Reservation', 42.651123, -70.786834, 'stavros-sm.jpg', 'stavros-reservation/', 10],
-        ['The Stevens-Coolidge Place', 42.681484, -71.117535, 'stevens-sm.jpg', 'the-stevens-coolidge-place/', 11],
-        ['Charles W. Ward Reservation', 42.639711, -71.113056, 'ward-sm.jpg', 'charles-w-ward-reservation/', 12],
-        ['Weir Hill', 42.697330, -71.110951, 'weirhill-sm.jpg', 'weir-hill/', 13],
+        //['The Stevens-Coolidge Place', 42.681484, -71.117535, 'stevens-sm.jpg', 'the-stevens-coolidge-place/', 11],
+        //['Charles W. Ward Reservation', 42.639711, -71.113056, 'ward-sm.jpg', 'charles-w-ward-reservation/', 12],
+        //['Weir Hill', 42.697330, -71.110951, 'weirhill-sm.jpg', 'weir-hill/', 13],
         ['Agassiz Rock', 42.598149, -70.766914, 'agassiz-sm.jpg', 'agassiz-rock/', 14],
+        ['Sally Weatherall Reservation', 42.701700, -70.804188, 'sally-sm.jpg', 'Sally-Weatherall-Reservation-and-Viewing-Platform', 15],
+        ['Allyn Cox Reservation', 42.633830, -70.764614, 'cox-sm.jpg', 'Allyn-Cox-Reservation', 16],
+        //['Donovan Reservation', 42.641327, -70.821890, 'donovan-sm.jpg', 'John-J-Donovan-Reservation-Sagamore-Hill-Conservation-Area', 17],
+        ['Harvey Reservation', 42.679187, -70.660575, 'harvey-sm.jpg', 'Harvey-Reservation', 18],
+        ['Tompson Street Reservation', 42.620, -70.724, 'tompson-sm.jpg', 'Tompson-Street-Reservation', 19],
+        ['Stoney Cove Presson Point', 42.625613, -70.704964, 'stonycove-sm.jpg', 'Stoney-Cove-and-Presson-Point', 20],
     ];
 
     // When the user clicks the marker, an info window opens.
 
     function initMap() {
-        var centerLatLng = {lat: 42.671888, lng: -70.907086};
+        var centerLatLng = {lat: 42.694674, lng: -70.821945};
 
         var map = new google.maps.Map(document.getElementById('map'), {
             zoom: 11,
@@ -28,8 +34,9 @@
              
         var i=0;
 
-        for (i = 0; i < properties.length; i++) {  
-            var infoContent = `<img src='assets/images/${properties[i][3]}'><p style="font-weight:bold">${properties[i][0]}</p><p>Scramble across ledges scarred for eons by wind and wave, and examine tide pools teeming with marine life.</p><a href="https://thetrustees.org/place/${properties[i][4]}">click here to read more</a>`;
+        for (i = 0; i < properties.length; i++) { 
+            
+            var infoContent = `<img src='assets/images/${properties[i][3]}'><p style="font-weight:bold">${properties[i][0]}</p>`;                            
             
             var marker = new google.maps.Marker({
                 position: new google.maps.LatLng(properties[i][1], properties[i][2]),
@@ -41,9 +48,18 @@
                 maxWidth: 200,              
                 });
 
-            google.maps.event.addListener(marker, 'click', function() { 
+            //google.maps.event.addListener(marker, 'click', function() { 
                
+                //this.info.open(this.getMap(), this);
+            //});
+
+            marker.addListener('mouseover', function() {
                 this.info.open(this.getMap(), this);
+            });
+
+//  hide the infowindow when user mouses-out
+            marker.addListener('mouseout', function() {
+                this.info.close();
             });
         }
     }
